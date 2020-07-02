2f6de1382d20e3ae2e591ad94f8be134fb797fec
ALVIN02-ui <alvinmwendwa96@gmail.com>
2020-07-02 16:49:25 +0300

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Iphone 11 pro max',
+    price: 799,
+    description: 'The iPhone 11 Pro and Pro Max feature Intels modems with Gigabit-class LTE, 4x4 MIMO, and LAA for speeds up to 1.6Gb/s, Wi-Fi 6 support (802.11ax), Bluetooth 5.0, Dual-SIM with eSIM, and an Apple-designed U1 Ultra Wideband chip that improves spatial awareness and allows for better indoor tracking'
+  },
+  {
+    name: 'samsung note ',
+    price: 899,
+    description: 'The Galaxy Note 10 Plus is Samsung biggest and most powerful phone yet, and its Aura colors almost symbolically reflect smartphone luxury top to bottom. The hand-stretching 6.8-inch display is perfectly paired with the S Pen and an extra-big battery'
+  },
+  {
+    name: 'Oppo f11 pro',
+    price: 699,
+    description: 'The phone is slightly bulky at 191g and is heavier than average. It packs in a 4,000mAh battery and has support for 20W fast charging. The Oppo F11 Pro is powered by a MediaTek Helio P70 SoC and comes with 6GB of RAM and 64GB of storage. It runs ColorOS 6 running on top of Android 9 Pie.'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
